c56cd4270ff6477db597a201b227bb576f7d3eea
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-06-18 11:56:46 -0300

    created getters and setters in entities question and answer

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -11,9 +11,46 @@ interface AnswerProps {
 }
 
 export class Answer extends Entity<AnswerProps> {
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get questionId() {
+    return this.props.questionId
+  }
+
   get content() {
     return this.props.content
   }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get excerpt() { // we can create a property that doesn't exist in the class's typing
+    return this.content.substring(0, 120).trimEnd().concat('...')
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  } 
+
+  /* set content(content: string) {
+    /* if (content.length > 2400) { // example of how we can create validation in set and get methods
+      throw new Error('Invalid content length!')
+    } */
+
+  //  this.props.content = content
+  // }
 
   static create(props: Optional<AnswerProps, 'createdAt'>, id?: UniqueEntityID) { // createdAt will be optional when creating a new question - but not database optional
     const answer = new Answer(
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -2,6 +2,7 @@ import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
 import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 import { Optional } from "../../core/types/optional"
+import dayjs from 'dayjs'
 
 interface QuestionProps {
   authorId: UniqueEntityID
@@ -14,9 +15,65 @@ interface QuestionProps {
 }
 
 export class Question extends Entity<QuestionProps> {
-  static create(props: Optional<QuestionProps, 'createdAt'>, id?: UniqueEntityID) { // createdAt will be optional when creating a new question - but not database optional
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId
+  }
+
+  get title() {
+    return this.props.title
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get slug() {
+    return this.props.slug
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get isNew(): boolean {// we can create a property that doesn't exist in the class's typing
+    return dayjs().diff(this.createdAt, 'days') <= 3
+  }
+
+  get excerpt() { // we can create a property that doesn't exist in the class's typing
+    return this.content.substring(0, 120).trimEnd().concat('...')
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  } 
+
+  set title(title: string) {
+    this.props.title = title
+    this.props.slug = Slug.createFromText(title)
+    this.touch()
+  } 
+
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+    this.props.bestAnswerId = bestAnswerId
+    this.touch()
+  } 
+
+  static create(props: Optional<QuestionProps, 'createdAt' | 'slug'>, id?: UniqueEntityID) { // createdAt will be optional when creating a new question - but not database optional
     const question = new Question(
-      {...props, createdAt: new Date()}, id // let's automate the creation of createdAt
+      {...props, slug: props.slug ?? Slug.createFromText(props.title), createdAt: new Date()}, id // let's automate the creation of createdAt and slug
     ) // we can call Question class because we extend Entity class
 
     return question
