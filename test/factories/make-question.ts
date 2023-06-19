a27f0e189685aa152b952a2b470fdc09dda7dcdc
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-06-19 18:07:19 -0300

    used framework faker in tests

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -3,16 +3,23 @@ import {
   Question,
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
+import { faker } from '@faker-js/faker'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
   // 'Partial' makes all properties in 'QuestionProps' optional
-  const question = Question.create({
-    title: 'Example',
-    // slug: Slug.create('example-question'), // automated creation
-    authorId: new UniqueEntityID(),
-    content: 'Example content',
-    ...override, // to overwrite any value that was sent in 'override'
-  })
+  const question = Question.create(
+    {
+      title: faker.lorem.sentence(), // 'sentence' created a text small
+      // slug: Slug.create('example-question'), // automated creation
+      authorId: new UniqueEntityID(),
+      content: faker.lorem.text(),
+      ...override, // to overwrite any value that was sent in 'override'
+    },
+    id,
+  )
 
   return question
 }
