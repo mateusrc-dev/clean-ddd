d8bd4f530ffde91a1a3a3d7c35a7ea908b074f5e
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-06-23 12:18:40 -0300

    create question with attachments

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { CreateQuestionUseCase } from './create-question'
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
 
@@ -15,11 +16,20 @@ describe('Create Question', () => {
       authorId: '1',
       title: 'Nova pergunta',
       content: 'Conteúdo da pergunta',
+      attachmentsIds: ['1', '2'],
     })
 
     expect(result.isRight()).toBe(true) // id cannot be undefined, null
     expect(inMemoryQuestionsRepository.items[0].id).toEqual(
       result.value?.question.id,
     )
+    expect(
+      inMemoryQuestionsRepository.items[0].attachments[0].attachmentId,
+    ).toEqual(new UniqueEntityID('1'))
+    expect(inMemoryQuestionsRepository.items[0].attachments).toHaveLength(2)
+    expect(inMemoryQuestionsRepository.items[0].attachments).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityID('1') }),
+      expect.objectContaining({ attachmentId: new UniqueEntityID('2') }),
+    ])
   })
 })
--- src/domain/forum/application/use-cases/create-question.ts
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -2,12 +2,14 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Question } from '../../enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/question-repository'
 import { Either, right } from '@/core/either'
+import { QuestionAttachment } from '../../enterprise/entities/question-attachment'
 
 interface CreateQuestionUseCaseRequest {
   // interface helps to identify what we are going to receive in this class as a parameter
   authorId: string
   title: string
   content: string
+  attachmentsIds: string[] // we let's have just the ids of attachments - multipart/form-data will have a route specific
 }
 
 type CreateQuestionUseCaseResponse = Either<null, { question: Question }>
@@ -19,12 +21,22 @@ export class CreateQuestionUseCase {
     authorId,
     content,
     title,
+    attachmentsIds,
   }: CreateQuestionUseCaseRequest): Promise<CreateQuestionUseCaseResponse> {
     const question = Question.create({
       authorId: new UniqueEntityID(authorId),
       content,
       title,
     })
+
+    const QuestionAttachments = attachmentsIds.map((attachmentId) => {
+      return QuestionAttachment.create({
+        attachmentId: new UniqueEntityID(attachmentId),
+        questionId: question.id,
+      })
+    })
+
+    question.attachments = QuestionAttachments
 
     await this.questionRepository.create(question)
 
