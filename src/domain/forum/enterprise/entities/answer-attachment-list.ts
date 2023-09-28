ece4fc3612aba285f209a42f689be5f4999e6c5e
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-09-28 15:03:34 -0300

    updated project

--- src/domain/forum/enterprise/entities/answer-attachment-list.ts
+++ src/domain/forum/enterprise/entities/answer-attachment-list.ts
@@ -4,6 +4,6 @@ import { AnswerAttachment } from './answer-attachment'
 export class AnswerAttachmentList extends WatchedList<AnswerAttachment> {
   // this generic says what kind of class we're going to have inside
   compareItems(a: AnswerAttachment, b: AnswerAttachment): boolean {
-    return a.attachmentId === b.attachmentId
+    return a.attachmentId.equals(b.attachmentId)
   }
 }
--- src/domain/forum/enterprise/entities/question-attachment-list.ts
+++ src/domain/forum/enterprise/entities/question-attachment-list.ts
@@ -4,6 +4,6 @@ import { QuestionAttachment } from './question-attachment'
 export class QuestionAttachmentList extends WatchedList<QuestionAttachment> {
   // this generic says what kind of class we're going to have inside
   compareItems(a: QuestionAttachment, b: QuestionAttachment): boolean {
-    return a.attachmentId === b.attachmentId
+    return a.attachmentId.equals(b.attachmentId)
   }
 }
